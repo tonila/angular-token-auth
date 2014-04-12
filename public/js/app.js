4fedbc82c44f35f9af5ac84a4166ad18b2ebb4c1
Toni Lahnalampi <toni.lahnalampi@livion.fi>
2014-04-12 22:12:49 +0300

    add resolve functions to routeprovider and remove $rootScope.isAuthenticated, since it is not needed.

--- public/js/app.js
+++ public/js/app.js
@@ -10,18 +10,25 @@ var myApp = angular.module('myApp', [
 
 myApp
 .config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+  var checkLoggedin = ['$q', '$timeout', '$window', '$location', function($q, $timeout, $window, $location) {
+    var deferred = $q.defer();
+    if ($window.sessionStorage.token) {
+      $timeout(deferred.resolve);
+    } else {
+      $timeout(deferred.reject);
+      $location.path('/login');
+    }
+    return deferred.promise;    
+  }]
   $httpProvider.interceptors.push('authInterceptor');
-  $routeProvider.when('/view1', {templateUrl: 'partials/view1.html', controller: 'View1Ctrl'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/view2.html', controller: 'View2Ctrl'});
+  $routeProvider.when('/view1', {templateUrl: 'partials/view1.html', controller: 'View1Ctrl', resolve: { loggedin: checkLoggedin }});
+  $routeProvider.when('/view2', {templateUrl: 'partials/view2.html', controller: 'View2Ctrl', resolve: { loggedin: checkLoggedin }});
   $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
   $routeProvider.otherwise({redirectTo: '/login'});
 }])
 .run(['$rootScope', '$location', '$window', '$http',
 function ($rootScope, $location, $window, $http) {
   $rootScope.logout = function () {
-    //$scope.welcome = '';
-    //$scope.message = '';
-    $rootScope.isAuthenticated = false;
     delete $window.sessionStorage.token;
     $location.path('./login');
   };
