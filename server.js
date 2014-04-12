55835c949d6e238da922856c31ae1ddc253abb51
Toni Lahnalampi <toni.lahnalampi@livion.fi>
2014-04-12 22:13:06 +0300

    add lorem ipsum.

--- server.js
+++ server.js
@@ -43,14 +43,14 @@ app.post('/authenticate', function (req, res) {
 app.get('/api/view1', function (req, res) {
   console.log('user ' + req.user.email + ' is calling /api/view1');
   res.json({
-    name: 'View1 Data'
+    name: 'View1 Data, that requires authentication: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   });
 });
 
 app.get('/api/view2', function (req, res) {
   console.log('user ' + req.user.email + ' is calling /api/view2');
   res.json({
-    name: 'View2 Data'
+    name: 'View2 Data, that requires authentication: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   });
 });
 
